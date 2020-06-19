6ff38b4a03b60b821e59f95c96de9d7906f445b8
이도은 <doeon93@naver.com>
2020-06-19 14:32:37 +0900

    fix : 음성인식 기능 추가 (아직 미완성)

--- app/src/main/java/com/example/dmemo/View/Memo/AddMemoActivity.java
+++ app/src/main/java/com/example/dmemo/View/Memo/AddMemoActivity.java
@@ -1,20 +1,31 @@
 package com.example.dmemo.View.Memo;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.dmemo.View.Main.MainActivity;
 import com.example.dmemo.R;
 import com.example.dmemo.Utils.DBHelper;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import butterknife.BindView;
@@ -39,12 +50,53 @@ public class AddMemoActivity extends AppCompatActivity implements AddMemoContrac
     @BindView(R.id.btn_add_memo_submit)
     Button btn_add_memo_submit;
 
+    @BindView(R.id.btn_add_memo_voice)
+    Button btn_add_memo_voice;
+
+
+    Intent intent;
+    SpeechRecognizer mRecognizer;
+    private final int MY_PERMISSIONS_RECORD_AUDIO = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_add_memo);
         ButterKnife.bind(this);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.RECORD_AUDIO)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO}, MY_PERMISSIONS_RECORD_AUDIO
+                );
+            }
+        }
+        intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR");
+
+        mRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        mRecognizer.setRecognitionListener(recognitionListener);
+
+
+
+        Button button = (Button) findViewById(R.id.btn_add_memo_voice);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mRecognizer.startListening(intent);
+            }
+        });
+
+
+
+
         // Edit Text 포커스 주고 키보드 올리기
         et_title.requestFocus();
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -97,4 +149,53 @@ public class AddMemoActivity extends AppCompatActivity implements AddMemoContrac
         startActivity(intent);
         finish();
     }
+
+    private RecognitionListener recognitionListener = new RecognitionListener() {
+        @Override
+        public void onReadyForSpeech(Bundle bundle) {
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+        }
+
+        @Override
+        public void onRmsChanged(float v) {
+        }
+
+        @Override
+        public void onBufferReceived(byte[] bytes) {
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+        }
+
+        @Override
+        public void onError(int i) {
+            Toast.makeText(getApplicationContext(),
+                    "어서 말씀 해 주세요!", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onResults(Bundle bundle) {
+            String key = "";
+            key = SpeechRecognizer.RESULTS_RECOGNITION;
+            ArrayList<String> mResult = bundle.getStringArrayList(key);
+
+            String[] rs = new String[mResult.size()];
+            mResult.toArray(rs);
+
+            et_content.setText(rs[0]);
+        }
+
+        @Override
+        public void onPartialResults(Bundle bundle) {
+        }
+
+        @Override
+        public void onEvent(int i, Bundle bundle) {
+        }
+    };
+
 }
